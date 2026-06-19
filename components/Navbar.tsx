b79c85e41031385ac58e5df6ad262191e0fdf043
Oves <oveskhan890@gmail.com>
2026-06-19 17:47:59 +0530

    feat: implement Navbar component with navigation, user authentication, and real-time notification support

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -475,7 +475,7 @@ export default function Navbar() {
             </div>
 
             {/* Nav Links with accordion sub-categories */}
-            <nav className="flex-1 overflow-y-auto p-4 space-y-1">
+            <nav className="flex-1 overflow-y-auto overscroll-contain touch-pan-y p-4 space-y-1">
               <Link
                 href="/"
                 onClick={() => setMobileOpen(false)}
